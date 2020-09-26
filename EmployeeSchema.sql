24eef0a4484783481fc433b9aa84eb22c7fcced4
Monika Kakuturu <monikakakuturu@gmail.com>
2020-09-25 20:12:22 -0700

    Created the database schema

--- EmployeeSchema.sql
+++ EmployeeSchema.sql
@@ -1,1 +1,26 @@
-Create database Employee_DB
+Create database Employee_DB;
+
+Use Employee_DB;
+
+Create Table Employe (
+    id int auto_increment not null,
+    first_name varchar(30) null,
+    last_name varchar(30) null,
+    role_id int,
+    manager_id int,
+    Primary Key (id)
+);
+
+Create Table Role (
+    id int auto_increment not null,
+    title varchar(30) null,
+    salery decimal,
+    department_id int,
+    Primary Key (id)
+);
+
+Create Table Department (
+    id int auto_increment not null,
+    name varchar(30),
+    Primary Key (id)
+);
